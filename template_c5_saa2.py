c48f04a0922c880aaec8db97557a8c5a87da04b8
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-19 10:32:22 +0530

    Create template_c5_saa2.py

---
+++ template_c5_saa2.py
@@ -0,0 +1,11 @@
+# Creating a list to store names of 5 friends 
+
+
+# Creating a list to store hobbies of 5 friends 
+
+
+
+# Creating a "for" loop to repeat 5 times
+
+  # Printing the friend's name and the corresponding hobby
+  
